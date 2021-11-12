dbb3f5b0235325230835e081dca32455783e3e26
Chi Nguyen <q.chi1099@gmail.com>
2021-11-12 12:32:56 -0600

    update driver

--- Driver.java
+++ Driver.java
@@ -1,11 +1,11 @@
 import java.io.*;
 import java.util.*;
 
-import javax.xml.namespace.QName;
+// import javax.xml.namespace.QName;
 import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.events.Attribute;
+// import javax.xml.stream.events.Attribute;
 import javax.xml.stream.events.Characters;
 import javax.xml.stream.events.EndElement;
 import javax.xml.stream.events.StartElement;
@@ -24,6 +24,45 @@ public class Driver {
         XMLEventReader eventReader = factory.createXMLEventReader(new FileReader(file));
         
         List<Point> points = new ArrayList<>();
-        Point cur = null;
+        Point point = null;
+        StringBuilder current = new StringBuilder();
+
+        // Checking the availability of the next tag
+        while (eventReader.hasNext()) {
+            XMLEvent xmlEvent = eventReader.nextEvent();
+            if (xmlEvent.isStartElement()) {
+                StartElement startElement = xmlEvent.asStartElement();
+                if ("Point".equalsIgnoreCase(startElement.getName().getLocalPart())) {
+                    point = new Point();
+                }
+
+                //Now everytime content tags are found; 
+                //Move the iterator and read data
+                if ("coordinates".equalsIgnoreCase(startElement.getName().getLocalPart())) {
+                    Characters nameDataEvent = (Characters) eventReader.nextEvent();
+                    current.append(nameDataEvent.getData());
+                    String[] coor = current.toString().split(",");
+                    Double longitude = Double.parseDouble(coor[0]);
+                    Double latitude = Double.parseDouble(coor[1]);
+                    Double altitude = Double.parseDouble(coor[2]);
+                    point.setLongitude(longitude);
+                    point.setLatitude(latitude);
+                    point.setAltitude(altitude);
+                }
+            }
+
+            if (xmlEvent.isEndElement())
+            {
+                EndElement endElement = xmlEvent.asEndElement();
+                 
+                //If employee tag is closed then add the employee object to list; 
+                //and be ready to read next employee data
+                if("Point".equalsIgnoreCase(endElement.getName().getLocalPart())) {
+                    points.add(point);
+                }
+            }
+        }
+        System.out.println(points);
+        input.close();
     }
 }
--- Point.java
+++ Point.java
@@ -2,13 +2,31 @@ import java.util.*;
 import java.io.*;
 
 public class Point {
-    private double longitude = 0;
-    private double latitude = 0;
-    private double altitude = 0;
+    private double longitude;
+    private double latitude;
+    private double altitude;
+
+    public Point() {
+        longitude = 0;
+        latitude = 0;
+        altitude = 0;
+    }
 
     public Point(double longitude, double latitude, double altitude) {
         this.longitude = longitude;
         this.latitude = latitude;
+        this.altitude = altitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setAltitude(double altitude) {
         this.altitude = altitude;
     }
 
